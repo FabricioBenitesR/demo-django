0d8d3d8e6b0fdd6732076ff0bce7eadd1b216d69
FabricioBenitesR <fabricio2006216@gmail.com>
2026-06-26 10:52:19 -0300

    parte 2 completa

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
 from .models import Mensagem
-
 
 def index(request):
     mensagens = Mensagem.objects.all()
     return render(request, "home/index.html", {"mensagens": mensagens})
+
+def sobre(request):
+    return render(request, "home/sobre.html")
